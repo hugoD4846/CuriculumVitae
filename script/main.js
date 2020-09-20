fa578eb60b7940e08cb7b2c565ef8efcdc689b69
Hugo DUTOIT <hugo.dutoit.etu@univ-lille.fr>
2020-09-20 14:51:13 +0200

    Fomations section

--- script/main.js
+++ script/main.js
@@ -1,4 +1,3 @@
-const wesh = document.querySelector(".competences");
 const plop = document.querySelectorAll("nav>ul ul");
 
 function navSetUp(){
@@ -6,24 +5,13 @@ function navSetUp(){
         for(let j=0;j<plop[i].children.length;j++){
             plop[i].children[j].style.transform = "translateY("+(-j*40)+"px)"
             plop[i].parentNode.addEventListener('mouseover',function(){
-                plop[i].children[j].style.transform = "translateY("+(j*9)+"px)"
+                plop[i].children[j].style.transform = "translateY(0px)"
             })
             plop[i].parentNode.addEventListener('mouseout',function(){
                 plop[i].children[j].style.transform = "translateY("+(-j*40)+"px)"
-                
             })
         }
     }
 }
 navSetUp();
-
-
-function randomColor(){
-        wesh.style.backgroundColor =
-            "rgb("+
-            Math.floor(Math.random()*255)+","+
-            Math.floor(Math.random()*255)+","+
-            Math.floor(Math.random()*255)+")";
-    ;
-}
 
